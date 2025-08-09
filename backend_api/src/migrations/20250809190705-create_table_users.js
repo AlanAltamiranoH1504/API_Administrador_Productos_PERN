4b97b6b2d08362342178b5541e84d0c930f43948
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-08-09 13:45:09 -0600

    feat(backend): add migration configuration with sequelize-cli - add .sequelizerc file to define migration location - add config.js file for necessary sequelize-cli configuration - add migration example for users table

---
+++ backend_api/src/migrations/20250809190705-create_table_users.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.createTable("usuarios", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            nombre: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            }
+        })
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.dropTable("usuarios")
+    }
+};
+
+/**
+ * TODO: COMANDO QUE SE DEBE EJECUTAR PARA TODAS LAS MIGRACIONES: npx sequelize-cli db:migrate
+ */
