966ce933f96d36bcb6e9a40ceb0e6792786b0d64
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-08-14 20:09:20 -0600

    feat: add documentation for model Category

--- backend_api/src/routers/ProductoRouter.ts
+++ backend_api/src/routers/ProductoRouter.ts
@@ -48,6 +48,26 @@ import {
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Categorias:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: El id de la categoria
+ *           example: 1
+ *         nombre:
+ *           type: string
+ *           description: El nombre de la categoria
+ *           example: Tecnologia
+ *         productos:
+ *           type: array
+ *           description: Array de productos que pertenecen a esa categoria
+ */
+
+/**
+ * @swagger
  * /productos:
  *      get:
  *          summary: Obtiene una lista de productos
@@ -63,6 +83,28 @@ import {
  *                              type: array
  *                              items:
  *                                  $ref: '#/components/schemas/Producto'
+ *              404:
+ *                  description: Productos no encontrados
+ *
+ */
+
+/**
+ * @swagger
+ * /categorias:
+ *      get:
+ *          summary: Obtiene una lista de categorias
+ *          tags:
+ *              - Categorias
+ *          description: Retorna una lista de categorias
+ *          responses:
+ *              200:
+ *                  description: Respuesta correcta
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Categorias'
  *              404:
  *                  description: Productos no encontrados
  *
@@ -92,6 +134,32 @@ import {
  *                              $ref: '#/components/schemas/Producto'
  *              400:
  *                  description: Producto no encontrado
+ */
+
+/**
+ * @swagger
+ * /categorias/{id}:
+ *      get:
+ *          summary: Obtiene una categoria por id
+ *          tags:
+ *              - Categorias
+ *          description: Retorna una categoria por su id
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                required: true
+ *                schema:
+ *                  type: integer
+ *                description: Id de la categoria
+ *          responses:
+ *              200:
+ *                  description: Categoria encontrado
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/components/schemas/Categorias'
+ *              400:
+ *                  description: Categoria no encontrado
  */
 
 /**
@@ -135,12 +203,39 @@ import {
  *                  description: Error en creacion de producto
  */
 
+/**
+ * @swagger
+ * /categorias:
+ *      post:
+ *          summary: Creacion de una categoria nueva
+ *          tags:
+ *              - Categorias
+ *          description: Guarda una categoria en la base de datos
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              nombre:
+ *                                  type: string
+ *                                  example: "Tecnologia"
+ *          responses:
+ *              200:
+ *                  description: Categoria guardada correctamente
+ *              409:
+ *                  description: Conflicto en body de request
+ *              500:
+ *                  description: Error en creacion de categoria
+ */
+
 
 /**
  * @swagger
  * /productos/{id}:
  *      put:
- *          summary: Actualizacion de un producto
+ *          summary: Actualizacion de un producto por su id
  *          tags:
  *              - Productos
  *          description: Actualiza un producto en la base de datos
@@ -182,6 +277,42 @@ import {
  *                  description: Categoria no encontrada
  *              500:
  *                  description: Error en creacion de producto
+ */
+
+/**
+ * @swagger
+ * /categorias/{id}:
+ *      put:
+ *          summary: Actualizacion de una categoria por su id
+ *          tags:
+ *              - Categorias
+ *          description: Actualiza una categoria en la base de datos
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                required: true
+ *                schema:
+ *                  type: integer
+ *                  description: Id de la categoria
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              nombre:
+ *                                  type: string
+ *                                  example: "Monitor curvo"
+ *          responses:
+ *              200:
+ *                  description: Categoria actualizada correctamente
+ *              409:
+ *                  description: Conflicto en body de request
+ *              404:
+ *                  description: Categoria no encontrada
+ *              500:
+ *                  description: Error en actualizacion de categoria
  */
 
 const router = express.Router();
