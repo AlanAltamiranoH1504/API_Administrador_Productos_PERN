dab3689206ba9c8534105af829932617dbcfe926
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-08-14 19:58:40 -0600

    feat: add documentation for product creation and updating

--- backend_api/src/routers/ProductoRouter.ts
+++ backend_api/src/routers/ProductoRouter.ts
@@ -26,8 +26,12 @@ import {
  *           example: 1
  *         nombre:
  *           type: string
- *           description: La descripción del producto
+ *           description: El nombre del producto
  *           example: Alexa Echo Dot 5
+ *         descripcion:
+ *           type: string
+ *           description: Descripcion del producto
+ *           example: Asistente virtual
  *         precio:
  *           type: number
  *           description: Precio del producto
@@ -38,7 +42,7 @@ import {
  *           example: true
  *         categoriaId:
  *           type: number
- *           descripcion: Foreign Key hacia la tabla Categorias
+ *           description: Foreign Key hacia la tabla Categorias
  *           example: 1
  */
 
@@ -63,11 +67,6 @@ import {
  *                  description: Productos no encontrados
  *
  */
-
-
-const router = express.Router();
-router.get("/prueba", accionPrueba);
-router.get("", listProductos);
 
 /**
  * @swagger
@@ -94,6 +93,100 @@ router.get("", listProductos);
  *              400:
  *                  description: Producto no encontrado
  */
+
+/**
+ * @swagger
+ * /productos:
+ *      post:
+ *          summary: Creacion de un producto nuevo
+ *          tags:
+ *              - Productos
+ *          description: Guarda un producto en la base de datos
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              nombre:
+ *                                  type: string
+ *                                  example: "Monitor curvo"
+ *                              descripcion:
+ *                                  type: string
+ *                                  example: "Monitor de 24 pulgadas"
+ *                              precio:
+ *                                  type: number
+ *                                  example: 1500
+ *                              disponible:
+ *                                  type: boolean
+ *                                  example: true
+ *                              categoriaId:
+ *                                  type: number
+ *                                  example: 1
+ *          responses:
+ *              200:
+ *                  description: Producto guardado correctamente
+ *              409:
+ *                  description: Conflicto en body de request
+ *              404:
+ *                  description: Categoria no encontrada
+ *              500:
+ *                  description: Error en creacion de producto
+ */
+
+
+/**
+ * @swagger
+ * /productos/{id}:
+ *      put:
+ *          summary: Actualizacion de un producto
+ *          tags:
+ *              - Productos
+ *          description: Actualiza un producto en la base de datos
+ *          parameters:
+ *              - in: path
+ *                name: id
+ *                required: true
+ *                schema:
+ *                  type: integer
+ *                  description: Id del producto
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              nombre:
+ *                                  type: string
+ *                                  example: "Monitor curvo"
+ *                              descripcion:
+ *                                  type: string
+ *                                  example: "Monitor de 24 pulgadas"
+ *                              precio:
+ *                                  type: number
+ *                                  example: 1500
+ *                              disponible:
+ *                                  type: boolean
+ *                                  example: true
+ *                              categoriaId:
+ *                                  type: number
+ *                                  example: 1
+ *          responses:
+ *              200:
+ *                  description: Producto actualizado correctamente
+ *              409:
+ *                  description: Conflicto en body de request
+ *              404:
+ *                  description: Categoria no encontrada
+ *              500:
+ *                  description: Error en creacion de producto
+ */
+
+const router = express.Router();
+router.get("/prueba", accionPrueba);
+router.get("", listProductos);
 router.get("/:id", FindByIdProductoRequest, findById);
 router.post("", CreateProductoRequest, saveProducto);
 router.put("/:id", UpdateProductoRequest, updateProducto);
