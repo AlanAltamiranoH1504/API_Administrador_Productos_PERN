4dac15ea26e5bfd47a84842f706f7dc8a27f878e
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-08-15 20:27:39 -0600

    feat (backend): add CORS configuration

--- backend_api/src/server.ts
+++ backend_api/src/server.ts
@@ -3,13 +3,17 @@ const app = express();
 import productoRouter from "./routers/ProductoRouter";
 import categoriasRouter from "./routers/CategoriasRouter";
 import conexionDB from "./configuration/db";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
+import {corsConfig} from "./configuration/cors";
 import swaggerSpec from "./configuration/swagger";
 app.use(express.json());
 //Prueba de conexion a db
 conexionDB.authenticate().then(() => {
     console.log("CORRECTA CONEXION A BASE DE DATOS");
 });
+//Definicion de cors de aplicacion
+app.use(cors(corsConfig));
 
 //Definicion de routers
 app.use("/productos", productoRouter);
